23d5a9b7e21d6927e288dc743166fdef645c9842
Federica Gaspari <34043707+Tars4815@users.noreply.github.com>
2023-12-05 16:20:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_rtd_theme',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
 html_static_path = ['_static']
