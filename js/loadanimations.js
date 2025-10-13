1579b361524854030cb76e7de0374da1848ae829
Federica Gaspari <34043707+Tars4815@users.noreply.github.com>
2025-10-13 22:40:21 +0200

    Refactor and document annotation and animation scripts
    Refactored js/loadanimations.js to modularize camera animation creation and added JSDoc comments. Improved js/loadannotations.js and js/loadphotos.js with enhanced documentation, code organization, and consistent formatting. Added .gitignore entry for pointclouds/arsenaletot and included a .gitkeep file for pointclouds directory.

--- js/loadanimations.js
+++ js/loadanimations.js
@@ -1,17 +1,46 @@
-//-------Definition of Animation 1
+/**
+ * Creates a Potree CameraAnimation from arrays of positions and targets.
+ *
+ * @param {Potree.Viewer} viewer The Potree viewer instance.
+ * @param {Array<Array<number>>} positions An array of [x, y, z] camera position arrays.
+ * @param {Array<Array<number>>} targets An array of [x, y, z] camera target arrays.
+ * @param {Potree.IndoorScene | undefined} indoor Optional indoor scene object to add the animation to.
+ * @returns {Potree.CameraAnimation} The created CameraAnimation object.
+ */
+const createCameraAnimation = (viewer, positions, targets, indoor) => {
+    const animation = new Potree.CameraAnimation(viewer);
 
-const animation = new Potree.CameraAnimation(viewer);
+    if (positions.length !== targets.length) {
+        console.error("Positions and targets arrays must have the same length.");
+        return animation;
+    }
 
+    for (let i = 0; i < positions.length; i++) {
+        const cp = animation.createControlPoint();
+        cp.position.set(...positions[i]);
+        cp.target.set(...targets[i]);
+    }
 
-const positions = [
+    if (indoor) {
+        indoor.addCameraAnimation(animation);
+    }
+    
+    // Set to invisible by default, as in your original script
+    animation.visible = false; 
+
+    return animation;
+};
+
+// --- Animation 1 Data ---
+// Coordinates of the camera positions and targets for the first animation
+const positions1 = [
     [553471.5649587561, 4988912.824383096, 96.75],
     [553468.5406591574, 4988910.128623269, 96.75],
     [553464.7474747964, 4988906.992126563, 96.75],
     [553461.7708505556, 4988905.213628605, 96.75],
     [553456.671652177, 4988904.093441524, 96.75]
 ];
-
-const targets = [
+const targets1 = [
     [553470.9753199244, 4988912.469004575, 96.55],
     [553466.6034696557, 4988908.379363626, 96.55],
     [553463.7472720352, 4988906.132089904, 96.55],
@@ -19,20 +48,7 @@ const targets = [
     [553456.3547397406, 4988904.085110492, 96.55]
 ];
 
-for (let i = 0; i < positions.length; i++) {
-    const cp = animation.createControlPoint();
-
-    cp.position.set(...positions[i]);
-    cp.target.set(...targets[i]);
-}
-
-indoor.addCameraAnimation(animation);
-animation.visible = false;
-
-//--------
-//-------Definition of Animation 2
-
-const animation2 = new Potree.CameraAnimation(viewer);
+// --- Animation 2 Data ---
 
 const positions2 = [
     [553471.5649587561, 4988912.824383096, 96.74929992934102],
@@ -50,12 +66,7 @@ const targets2 = [
     [553464.0111806979, 4988932.023884267, 96.57968306104952]
 ];
 
-for (let i = 0; i < positions2.length; i++) {
-    const cp = animation2.createControlPoint();
+// --- Animation Creation ---
 
-    cp.position.set(...positions2[i]);
-    cp.target.set(...targets2[i]);
-}
-
-indoor.addCameraAnimation(animation2);
-animation2.visible = false;
+const animation1 = createCameraAnimation(viewer, positions1, targets1, indoor);
+const animation2 = createCameraAnimation(viewer, positions2, targets2, indoor);
